a67ea793c2b56d167e703e255c8094bef149fd39
Gabriel Vieira <gabkv99@hotmail.com>
2024-11-27 10:34:07 -0300

    usando redux toolkit

---
+++ src/store/reducers/favoritos.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+import { Produto } from '../../App'
+
+type FavoritosState = {
+  itens: Produto[]
+}
+
+const initialState: FavoritosState = {
+  itens: []
+}
+
+const FavoritosSlice = createSlice({
+  name: 'favoritos',
+  initialState,
+  reducers: {
+    favoritar: (state, action: PayloadAction<Produto>) => {
+      const produto = action.payload
+
+      if (state.itens.find((p) => p.id === produto.id)) {
+        alert('Item já adicionado')
+      } else {
+        state.itens.push(produto)
+      }
+    }
+  }
+})
+
+export const { favoritar } = FavoritosSlice.actions
+export default FavoritosSlice.reducer
