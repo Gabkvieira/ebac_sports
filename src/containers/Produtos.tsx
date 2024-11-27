8d3090bda1c040d926ebf6f3d65006bf08800707
Gabriel Vieira <gabkv99@hotmail.com>
2024-11-27 10:51:41 -0300

    alterando Produtos tsx

--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,30 +1,17 @@
 import { Produto as ProdutoType } from '../App'
 import Produto from '../components/Produto'
+import { useGetProdutosQuery } from '../services/api'
 
 import * as S from './styles'
 
-type Props = {
-  produtos: ProdutoType[]
-  favoritos: ProdutoType[]
-}
-
-const ProdutosComponent = ({ produtos, favoritos }: Props) => {
-  const produtoEstaNosFavoritos = (produto: ProdutoType) => {
-    const produtoId = produto.id
-    const IdsDosFavoritos = favoritos.map((f) => f.id)
-
-    return IdsDosFavoritos.includes(produtoId)
-  }
+const ProdutosComponent = () => {
+  const { data: produtos } = useGetProdutosQuery()
 
   return (
     <>
       <S.Produtos>
-        {produtos.map((produto) => (
-          <Produto
-            estaNosFavoritos={produtoEstaNosFavoritos(produto)}
-            key={produto.id}
-            produto={produto}
-          />
+        {produtos?.map((produto) => (
+          <Produto key={produto.id} produto={produto} />
         ))}
       </S.Produtos>
     </>
